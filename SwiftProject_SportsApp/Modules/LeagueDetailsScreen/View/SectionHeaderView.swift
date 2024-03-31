c1c2cc7d6008151fc3dc8a1e96570ca78eddbb7d
A-DME <201727138@std.sci.cu.edu.eg>
2024-03-31 21:40:07 +0200

    tryin' to put section header in league Details

--- SwiftProject_SportsApp/Modules/LeagueDetailsScreen/View/SectionHeaderView.swift
+++ SwiftProject_SportsApp/Modules/LeagueDetailsScreen/View/SectionHeaderView.swift
@@ -10,14 +10,4 @@ import UIKit
 class SectionHeaderView: UICollectionReusableView {
         
     @IBOutlet weak var headerLabel: UILabel!
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        addSubview(headerLabel)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
